be71b5a32f8199db4f5c893788d344953510c97d
姜泉 <595121869@qq.com>
2014-04-28 02:14:13 +0800

    创建数据访问接口和实现类
    创建数据访问接口和实现类

---
+++ src/com/dao/impl/UserDAOImpl.java
@@ -0,0 +1,50 @@
+package com.dao.impl;
+
+import com.dao.UserDAO;
+import com.bean.User;
+
+import java.util.List;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class UserDAOImpl extends HibernateDaoSupport implements UserDAO {
+	public void delete(Integer id){
+		//TOOD Auto-generated method stub
+		getHibernateTemplate().delete(getHibernateTemplate().get(User.class, id));
+	}
+	
+	public void delete(User user){
+		getHibernateTemplate().delete(user);
+	}
+	
+	public List<User> findAll(){
+		return getHibernateTemplate().find("from test");
+	}
+	
+	public User findUserByNameAndPass(String username,String password){
+		System.out.println("验证用");
+		//delete(2)；
+		//String() args=(username,password);
+		System.out.println("成功删除");
+		List ul =getHibernateTemplate().find("from com.bean.User au where au.username = '"+username+"'and au.password = '"+password+"'");
+		
+		if(ul.size()==1)
+		{
+			System.out.println("OK 123abc");
+			return (User)ul.get(0);
+		}
+		return null;
+	}
+	
+	public User get(Integer id){
+		return (User)getHibernateTemplate().get(User.class, id);
+	}
+	
+	public void save(User user){
+		getHibernateTemplate().save(user);
+	
+	}
+	public void update(User user){
+		getHibernateTemplate().saveOrUpdate(user);
+	}
+
+}
